d0f8d304865ffabf8f9ea143ee972bb7279ab454
Pedro Koike <ps.koike@gmail.com>
2017-05-12 14:53:46 -0300

    admin front designed

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :projects
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'projects#index'
+  get '/admin', to: 'projects#admin'
 end
