0a1f179503c548a612e35a1f918b8ccaa8e4a35e
Pedro Koike <ps.koike@gmail.com>
2017-05-15 17:45:36 -0300

    Trailer URl added to view, admin and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512180851) do
+ActiveRecord::Schema.define(version: 20170515202344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,9 +37,10 @@ ActiveRecord::Schema.define(version: 20170512180851) do
     t.string   "director"
     t.string   "job"
     t.integer  "year"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "image_url"
+    t.string   "trailer_url"
   end
 
 end
